979a0f07d95ca5e2306cb96bb9f9447081712c16
Bas Westerbaan <bas@westerbaan.name>
2024-07-10 13:08:31 +0200

    Add arm64 and test against generic

---
+++ and_arm64.go
@@ -0,0 +1,16 @@
+// go:build arm64
+package and
+
+//go:noescape
+func andNEON(dst, a, b *byte, len uint64)
+
+func and(dst, a, b []byte) {
+	l := uint64(len(a)) >> 8
+	if l != 0 {
+		andNEON(&dst[0], &a[0], &b[0], l)
+	}
+	l <<= 8
+	for i := l; i < uint64(len(a)); i++ {
+		dst[i] = a[i] & b[i]
+	}
+}
--- and_test.go
+++ and_test.go
@@ -1,8 +1,32 @@
 package and
 
 import (
+	"bytes"
+	"math/rand/v2"
 	"testing"
 )
+
+func testAgainstGeneric(t *testing.T, size int) {
+	a := make([]byte, size)
+	b := make([]byte, size)
+	c1 := make([]byte, size)
+	c2 := make([]byte, size)
+	And(c1, a, b)
+	andGeneric(c2, a, b)
+	if !bytes.Equal(c1, c2) {
+		t.Fatal()
+	}
+}
+
+func TestAgainstGeneric(t *testing.T) {
+	for i := 0; i < 20; i++ {
+		size := 1 << i
+		testAgainstGeneric(t, size)
+		for j := 0; j < 10; j++ {
+			testAgainstGeneric(t, size+rand.IntN(100))
+		}
+	}
+}
 
 func BenchmarkAnd(b *testing.B) {
 	b.StopTimer()
